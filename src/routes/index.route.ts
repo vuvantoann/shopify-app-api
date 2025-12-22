c9fff31f1670ea7aace6bd4148264e9f7b0916a3
vuvantoann <toanvu1728@gamil.com>
2025-12-22 15:17:28 +0700

    Tích hợp cors

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import shopRoutes from './shop.route'
+import cors from 'cors'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.use('/shop', shopRoutes)
